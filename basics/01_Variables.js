2f6b053f60eb410abcaa0cd2d46eac0f3b43a825
Amarjeet Prakash <70009763+Amarjeet2000@users.noreply.github.com>
2024-04-20 14:21:49 +0000

    Variables config done

---
+++ basics/01_Variables.js
@@ -0,0 +1,15 @@
+const accountId = 1
+let accountEmail = "amarjeetprakash@gmail.com"
+var accountPassword = "12345678"
+accountCity = "Bengaluru"
+
+accountEmail = "amarjeetprakash123@gmail.com"
+accountPassword = "12121212"
+accountCity = "Noida"
+
+// NOTE :- We prefer not to use Var in Javascript because it makes some issues while working for block scope and functional scope.
+
+let accountState;
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
